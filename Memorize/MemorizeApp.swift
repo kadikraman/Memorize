5847f28a165d2308de4ed2f76db4f6644b87354b
Kadi Kraman <hellokadi@gmail.com>
2021-02-07 12:21:15 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kadi Kraman on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
